7a6dae0f5e8bdb65653b54232511302739524cb9
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-11-10 12:25:12 +1300

    get lyrics asyn funtion

--- src/index.js
+++ src/index.js
@@ -20,7 +20,12 @@ async function getLyrics(artist, songTitle) {
     const res = await fetch (`${apiURL}/v1/${artist}/${songTitle}`)
     const data = await res.json()
 
-    const lyrics = data.lyrics.replace()
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+    `
+    more.innerHTML = ''
 }
 
 function showData (data) {
