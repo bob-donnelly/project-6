6ba81f2570267eaeaca89a23055167f199f7e27b
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-11-10 11:22:12 +1300

    made a link to api in js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// DOM selector
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const mroe = document.getElementById('more')
+
+const apiURL = 'http://api.lyrics.ovh'
+
+async function searchSongs (term) {
+    
+}
